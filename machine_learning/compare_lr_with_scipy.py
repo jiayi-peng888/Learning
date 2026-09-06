f3fdfe5c3ed8f1f749122922b9c8c51e79956196
jiayi-peng888 <1569560757@qq.com>
2026-09-06 16:56:44 +0800

    Update compare_lr_with_scipy.py

--- machine_learning/compare_lr_with_scipy.py
+++ machine_learning/compare_lr_with_scipy.py
@@ -1,5 +1,7 @@
-"""https://introml.mit.edu/notes/regression.html
-https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html"""
+"""
+https://introml.mit.edu/notes/regression.html
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html
+"""
 import numpy as np
 from ucimlrepo import fetch_ucirepo
 import matplotlib.pyplot as plt
